267854b1248e5f501a25295012ad0590d0ecd76e
Matheus Cavalcanti Rebola <m.cavalcantir@outlook.com>
2023-11-13 22:00:34 -0300

    first commit

---
+++ src/main/java/io/farma/FarmaciaApiApplication.java
@@ -0,0 +1,13 @@
+package io.farma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaApiApplication.class, args);
+	}
+
+}
